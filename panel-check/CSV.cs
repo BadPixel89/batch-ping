f899156475b27f9c863950d030db83c62bee83c5
daniel <danielreubenkelly@gmail.com>
2026-06-13 21:22:37 +0100

    initial commit of panel-check

---
+++ panel-check/CSV.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace panel_check
+{
+    internal class CSV
+    {
+        /// <summary>
+        /// Read the specified CSV file
+        /// </summary>
+        /// <param name="filePath"></param><br></br>
+        /// <returns>A list<string> of the lines in the csv doc</returns>
+        public List<string> ParseLines(string filePath)
+        {
+            string line = "";
+            List<string> lines = new List<string>();
+
+            try
+            {
+
+                using (StreamReader reader = new StreamReader(filePath))
+                {
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        lines.Add(line);
+                    }
+                }
+
+                return lines;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Write a csv file from a List<string> of CSV lines. If the lines are not comma separated they will still be written.
+        /// </summary>
+        /// <param name="csvLines"></param>
+        /// <param name="filePath"></param>
+        public void Write(List<string> csvLines, string filePath)
+        {
+            using (StreamWriter writer = new StreamWriter(filePath, false))
+            {
+                foreach (string line in csvLines)
+                {
+                    writer.WriteLine(line);
+                }
+            }
+            return;
+        }
+        public void Write(string csvLines, string filePath)
+        {
+            using (StreamWriter writer = new StreamWriter(filePath, false))
+            {
+                writer.Write(csvLines);
+            }
+            return;
+        }
+    }
+}
---
+++ panel-check/Program.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace panel_check
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ping _ping = new Ping();
+            PingOptions _options = new PingOptions();
+            PingReply _pingReply;
+            byte[] _buffer = Encoding.ASCII.GetBytes("aaaaaaaapalmlabchecksaaaaaaaaaaa");
+            int _timeout = 120;
+            List<string> _offlineRooms = new List<string>();
+
+            CSV _csv = new CSV();
+            string infile = AppDomain.CurrentDomain.BaseDirectory + "panels.csv";
+            string outfile = AppDomain.CurrentDomain.BaseDirectory + "panels-status.csv";
+
+            
+            List<string> csvData = _csv.ParseLines(infile);
+            if (csvData == null || csvData.Count == 0)
+            {
+                Console.WriteLine("[error] : panels.csv not found or contains no data. filepath checked : " + infile);
+                Console.WriteLine("press enter key to quit");
+                Console.Read();
+                return;
+            }
+            int ipindex = GetIPIndex(csvData[0]);
+            //add headers to output data
+            _offlineRooms.Add(csvData[0]);
+            
+            foreach (string line in csvData.Skip(1))
+            {
+                if (line == "")
+                {
+                    continue;
+                }
+                try
+                {
+                    _pingReply = _ping.Send(line.Split(',')[ipindex], _timeout, _buffer, _options);
+                    if (_pingReply.Status == IPStatus.Success)
+                    {
+                        continue;
+                    }
+                    _offlineRooms.Add(line);
+                    Console.WriteLine(line);
+                }
+                catch (Exception) {
+                    _offlineRooms.Add(line);
+                    Console.WriteLine(line);
+                }
+                
+            }
+            _csv.Write(_offlineRooms, outfile);
+            Console.WriteLine("[done] output file : "+outfile + "\r\npress enter key to quit");
+            Console.ReadLine();
+        }
+
+        private static string IncrementStatus(string statusbar)
+        {
+            if (statusbar.Length == 21)
+            {
+                return "[checking]           ";
+            }
+            else
+            {
+                return statusbar + ".";
+            }
+        }
+
+        private static int GetIPIndex(string headers)
+        {
+            string[] columns = headers.Split(',');
+
+            for (int i = 0; i < columns.Length; i++)
+            {
+                if (columns[i] == "IP")
+                {
+                    return i;
+                }
+            }
+            throw new Exception("[error] IP field not found");
+        }
+    }
+}
